9a381be69d1dd6902ea500546c561f7e9d9123f0
milaaano <gilyazetdinov06@gmail.com>
2025-08-07 16:31:24 +0300

    added storage in array, added style to messages

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const http = require('http');
 const fs = require('fs');
 
+let chat_history = [];
+
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         fs.readFile(__dirname + '/index.html', (err, data) => {
@@ -14,13 +16,20 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
-    }
-
-    if (req.url === '/style.css') {
+    } else if (req.url === '/style.css') {
         fs.readFile(__dirname + '/style.css', (err, css) => {
             if (err) {
                 res.writeHead(500);
                 return res.end('Error loading style.css');
+            }
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(css);
+        });
+    } else if (req.url === '/message.css') {
+        fs.readFile(__dirname + '/message.css', (err, css) => {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading message.css');
             }
             res.writeHead(200, { 'Content-Type': 'text/css' });
             res.end(css);
@@ -33,12 +42,12 @@ const io = require('socket.io')(server);
 const port = 4000;
 
 io.on('connection', (socket) => {
-    socket.on('send name', (user) => {
-        io.emit('send name', user);
-    });
+    socket.emit('send history', chat_history);
 
-    socket.on('send message', (chat) => {
-        io.emit('send message', chat);
+    socket.on('send message', (msg) => {;
+        const entry = {name: msg.name, message: msg.message, timestamp: new Date() };
+        chat_history.push(entry);
+        io.emit('send message', entry);
     });
 });
 
